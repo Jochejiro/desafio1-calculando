d34be07ceba4fb9eae3c7a2c8d52352f71adaf92
Jose Albornoz <prueba@outlook.cl>
2023-06-08 15:38:20 -0400

    Se añade codigo de validacion al input de cantidad

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 let btnCalcular = document.querySelector("button");
 
-btnCalcular.addEventListener("click", function () {
-    let valorCantidad = Number(document.querySelector("#cantidad").value);
-    let valorPrecio = Number(document.querySelector("#precio").innerHTML);
+btnCalcular.addEventListener("click", () => {
+    let inputCantidad = document.querySelector("#cantidad");
+    const valorCantidad = parseFloat(inputCantidad.value);
+    const valorPrecio = parseInt(document.querySelector("#precio").innerHTML);
     let valorColor = document.querySelector("#color").value;
 
-    document.querySelector("#result-total").innerHTML =
-        valorCantidad * valorPrecio;
-    document.querySelector("#result-cantidad").innerHTML = valorCantidad;
-    document.querySelector("#result-color").style.backgroundColor = valorColor;
+    console.log(valorCantidad % 1 == 0);
+
+    if (!isNaN(valorCantidad) && (valorCantidad % 1 == 0) && valorCantidad >= 0) {
+        inputCantidad.style.border = "none";
+        document.querySelector("#result-total").innerHTML = valorCantidad * valorPrecio;
+        document.querySelector("#result-cantidad").innerHTML = valorCantidad;
+        document.querySelector("#result-color").style.backgroundColor = valorColor;
+    } else {
+        inputCantidad.value = "";
+        inputCantidad.style.border = "2px solid red";
+        inputCantidad.placeholder = "Ingrese un numero entero.";
+    }
 });
